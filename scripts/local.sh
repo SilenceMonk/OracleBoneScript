371874980138293e6413398571390290310f9847
Silence-Monk <1209943123@qq.com>
2021-12-06 23:24:39 +0800

    init commit

--- scripts/local.sh
+++ scripts/local.sh
@@ -1,4 +1,4 @@
-export cfg_dir=cfgs/rough_resnet18_pretrained.yaml # config file dir
+export cfg_dir=cfgs/fine_resnet18_pretrained.yaml # config file dir
 export ngpu=1 bs=64 ddp=~ # number of gpus, batchsize per gpu, ddp mode (normal, slurm or ~)
 export seed=42 # reset random seed for reproduction
-python rough_test.py
+python fine.py
